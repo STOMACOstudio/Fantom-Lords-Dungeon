2c8e99c3aa1b4ae7741a7f2835bb7fc029885c62
Uriel Romeo <uromeo.soc@gmail.com>
2022-09-26 16:32:45 +0200

    custom_scripts.js

--- FantomLords_CharacterBuilder/extensions/web3/custom_script.js
+++ FantomLords_CharacterBuilder/extensions/web3/custom_script.js
@@ -29,21 +29,9 @@ async function getMetamaskAccount() {
 }
 
 
-async function getTokenBalance(wallet_address, token_address) {
-
-	// Default structure of ERC20 smart contract
+async function getUserFantomLords() {
+	// contract address: 0xfee8077c909d956E9036c2d2999723931CeFE548
 	let minABI = [{
-	    "inputs": [{
-    		"name": "_owner",
-    		"type":"address"
-    	}],
-	    "name":"balanceOf",
-	    "outputs":[{
-	    	"name":"balance",
-	    	"type":"uint256"
-	    }],
-	    "type":"function"
-	}, {
   		"inputs": [{
   			"internalType":"address",
   			"name":"_owner",
@@ -59,10 +47,38 @@ async function getTokenBalance(wallet_address, token_address) {
 		"type":"function"
 	}];
 
+	let contract = new web3.eth.Contract(minABI, "0xfee8077c909d956E9036c2d2999723931CeFE548");
+
+	let tokenIDs = [];
+	try {
+		tokenIDs = await contract.methods.walletOfOwner(wallet_address).call();
+	} catch(e) {
+		console.error(e);
+	}
+	return tokenIDs;
+}
+
+
+async function getTokenBalance(wallet_address, token_address) {
+
+	// Default structure of ERC20 smart contract
+	let minABI = [{
+	    "inputs": [{
+    		"name": "_owner",
+    		"type":"address"
+    	}],
+	    "name":"balanceOf",
+	    "outputs":[{
+	    	"name":"balance",
+	    	"type":"uint256"
+	    }],
+	    "type":"function"
+	}];
+
 	var map = {};
 	map["id"] = "getTokenBalance";
 	map["balance"]="-1";
-	map["tokenIDs"] = [];
+	map["tokenIDs"] = await getUserFantomLords();
 
 	let contract = new web3.eth.Contract(minABI, token_address);
 	console.log(contract);
@@ -70,7 +86,6 @@ async function getTokenBalance(wallet_address, token_address) {
 	try {
 
 		let balance = await contract.methods.balanceOf(wallet_address).call();
-		let tokenIDs = await contract.methods.walletOfOwner(wallet_address).call();
 		//const decimalPlaces = await contract.methods.decimals().call(); // 8
 		//let newBalance = 0;
 		const newBalance = (balance / 1e18).toFixed(3);
@@ -83,7 +98,6 @@ async function getTokenBalance(wallet_address, token_address) {
 		//console.log(decimalPlaces);
 		//console.log(newBalance);
 
-		map["tokenIDs"] = tokenIDs;
 		map["balance"] = newBalance;
 
 	} catch(error) {
