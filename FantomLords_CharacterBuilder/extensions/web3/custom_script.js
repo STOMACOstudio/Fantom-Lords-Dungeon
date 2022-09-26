2ccfc0355bba0792c5af27fa540b405e48accaa9
Uriel Romeo <uromeo.soc@gmail.com>
2022-09-26 16:29:03 +0200

    custom_scripts.js

--- FantomLords_CharacterBuilder/extensions/web3/custom_script.js
+++ FantomLords_CharacterBuilder/extensions/web3/custom_script.js
@@ -59,33 +59,6 @@ async function getTokenBalance(wallet_address, token_address) {
 		"type":"function"
 	}];
 
-	let minABI = [
-	  // balanceOf
-	  {
-	    "constant":true,
-	    "inputs":[{"name":"_owner","type":"address"}],
-	    "name":"balanceOf",
-	    "outputs":[{"name":"balance","type":"uint256"}],
-	    "type":"function"
-	  },
-	  // walletOfOwner
-	  {
-  		"inputs":[{"internalType":"address","name":"_owner","type":"address"}],
-		"name":"walletOfOwner",
-		"outputs":[{"internalType":"uint256[]","name":"","type":"uint256[]"}],
-		"stateMutability":"view",
-		"type":"function"
-      }
-	  // decimals
-	  /*{
-	    "constant":true,
-	    "inputs":[],
-	    "name":"decimals",
-	    "outputs":[{"name":"","type":"uint8"}],
-	    "type":"function"
-	  }*/
-	];
-
 	var map = {};
 	map["id"] = "getTokenBalance";
 	map["balance"]="-1";
